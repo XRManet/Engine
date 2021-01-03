8070e02f73f3c6964e6077a85a9bd3e444277940
Jiman Jeong <sixzone11@gmail.com>
2021-01-04 00:03:02 +0900

    lock에 대해 플랫폼 비종속적인 표준 구현을 기본으로 채택. 플랫폼 별 성능 비교하여 사용 여부 결정하면 될 것.

--- XRFrameworkBase/XRLock.cpp
+++ XRFrameworkBase/XRLock.cpp
@@ -1,5 +1,37 @@
 #include "stdafx.h"
 
+#include <XRFrameworkBase/XRLock.h>
+
+#include <XRFrameworkBase/XRPlatform.h>
+#include <mutex>
+
 namespace xr
 {
+#if defined(XR_PLATFORM_OSX)
+MutexLock::MutexLock()
+{
+	std::mutex& mutex = reinterpret_cast<std::mutex&>(_mutex);
+	new (&mutex) std::mutex;
+}
+
+MutexLock::~MutexLock()
+{
+	std::mutex& mutex = reinterpret_cast<std::mutex&>(_mutex);
+	mutex.~mutex();
+}
+
+bool MutexLock::lock()
+{
+	std::mutex& mutex = reinterpret_cast<std::mutex&>(_mutex);
+	mutex.lock();
+	return true;
+}
+
+bool MutexLock::unlock()
+{
+	std::mutex& mutex = reinterpret_cast<std::mutex&>(_mutex);
+	mutex.unlock();
+	return true;
+}
+#endif
 }
