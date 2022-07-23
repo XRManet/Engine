9f0f185da9d306cfd3266a8946cfe9098082b911
Jiman Jeong <sixzone11@gmail.com>
2022-07-23 16:47:39 +0900

    OSX glCompileShaderIncludeARB 구현 이상 문제로 관련 작업 중단

--- XRSourceBuildSystemGLSL/pch.cpp
+++ XRSourceBuildSystemGLSL/pch.cpp
@@ -3,3 +3,19 @@
 #include "pch.h"
 
 // When you are using pre-compiled headers, this source file is necessary for compilation to succeed.
+
+char const* GetGlErrorString(GLenum errorCode)
+{
+	static char stringifiedErrorCode[32] = { 0, };
+
+	switch (errorCode)
+	{
+	case GL_INVALID_ENUM:           return "GL_INVALID_ENUM";
+	case GL_INVALID_VALUE:          return "GL_INVALID_VALUE";
+	case GL_INVALID_OPERATION:      return "GL_INVALID_OPERATION";
+	default:
+		sprintf_s(stringifiedErrorCode, 31, "0x%x", errorCode);
+		return stringifiedErrorCode;
+	}
+	return nullptr;
+}
